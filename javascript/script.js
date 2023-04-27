93e85c481175b9de3e5ae99e5850a13f8c2f3b53
gmendez <mendezgd88@gmail.com>
2023-04-27 13:04:37 -0300

    imagen rota2

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@ const arrProdFijo = [
     { id: 1, nombre: "Ak47", stock: 10, precio: 2500, img: "prod1.jpg", descripcion: "El AK-47 es un fusil de asalto soviético diseñado por Mijaíl Kaláshnikov en 1947" },
     { id: 2, nombre: "M4A1", stock: 5, precio: 3100, img: "asalto1.jpg", descripcion: "La M4A1 es una carabina de asalto estadounidense que utiliza munición 5,56." },
     { id: 3, nombre: "Desert Eagle", stock: 22, precio: 700, img: "prod3.jpg", descripcion: "La Desert Eagle es una pistola semiautomática capacidad para disparar cartucho .50 Action Express." },
-    { id: 4, nombre: "Sniper AWP", stock: 10, precio: 5700, img: "awp.jpg", descripcion: "El AWP es un rifle de francotirador de cerrojo que utiliza munición calibre .338" },
+    { id: 4, nombre: "Sniper AWP", stock: 10, precio: 5700, img: "awp2.jpg", descripcion: "El AWP es un rifle de francotirador de cerrojo que utiliza munición calibre .338" },
     { id: 5, nombre: "Chaleco anti-balas", stock: 50, precio: 1000, img: "chaleco1.jpg", descripcion: "El Kevlar es un material sintético resistente y ligero que se utiliza en la fabricación de chalecos antibalas" },
     { id: 6, nombre: "Escopeta", stock: 40, precio: 1200, img: "escopetas1.jpg", descripcion: "Las escopetas están diseñadas para descargar varios proyectiles, como perdigones o postas, en cada disparo." },
     { id: 7, nombre: "Optica", stock: 300, precio: 150, img: "opticas1.jpg", descripcion: "Los visores son dispositivos ópticos que se acoplan al arma para mejorar la precisión del disparo." },
@@ -42,7 +42,7 @@ function crearCard(el) {
     let htmlCard = `
                 <div class="col">
                 <div class="card" style="width: 18rem;">
-                    <img src="./media/${el.img}" alt="${el.nombre} class="card-img-top" width="288px" height="250px" alt="...">
+                    <img src="./media/${el.img}" alt="${el.nombre} class="card-img-top" width="288px" height="250px">
                     <div class="card-body">
                         <h5 class="card-title">${el.nombre}</h5>
                         <p class="card-text">${el.descripcion}</p>
